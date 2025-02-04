7b82b7cad390aba226760ebc7e54311a68cc777d
thulanigamtee <thulanigamtee@gmail.com>
2025-02-04 03:04:07 +0200

    update production environment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,8 +1,10 @@
-const BACKEND_URL = 'https://tee.spotify-clone';
+// const BACKEND_URL = 'https://tee.spotify-clone';
 
 export const environment = {
-  BACKEND_URL: BACKEND_URL,
-  API_URL: `${BACKEND_URL}/api`,
+  // BACKEND_URL: BACKEND_URL,
+  // API_URL: `${BACKEND_URL}/api`,
+  production: true,
+  API_URL: 'https://spotify-clone-front-chi.vercel.app/',
   AUTH0_CLIENT_ID: 'MWfW75V886mwzZ614uBryJpLtqymLTJE',
   AUTH0_DOMAIN: 'https://dev-l7dg8uux7qwsjeod.us.auth0.com/',
 };
